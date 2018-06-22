844fbcb8391fe2cbf98e8c2d80fe2ec9b4680e53
benonymus <bence1204@gmail.com>
2018-06-22 12:59:36 +0700

    profile buttons

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { 
-  View, 
-  StyleSheet, 
-  Image 
-} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Button
+}
+from 'react-native';
+import {
+  Actions
+}
+from 'react-native-router-flux'
 
 const styles = StyleSheet.create({
   container: {
@@ -20,6 +26,10 @@ const styles = StyleSheet.create({
   }
 });
 
+const ButtonWrapper = styled.View `
+  width: 100%;
+`;
+
 class Profile extends React.Component {
   render() {
     return (
@@ -28,6 +38,27 @@ class Profile extends React.Component {
           style={styles.avatar}
           source={require('../assets/images/avatar.png')}
         />
+        <ButtonWrapper>
+          <Button
+            title='Manage your KinX'
+            color='#841584'
+            onPress={() => Actions.kinklist()}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button
+            title='Sumbit guest Kinx'
+            color='#841584'
+            onPress={() => Actions.kinklist()}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button
+            title='Most Recent Result'
+            color='#841584'
+            onPress={() => Actions.kinkcomparison()}
+          />
+        </ButtonWrapper>
       </View>
     );
   }
