77a38075ad3b32c63eeddfe1bd7fae86d01180e4
benonymus <bence1204@gmail.com>
2018-07-16 14:33:21 +0700

    a

--- app/api/Auth.js
+++ app/api/Auth.js
@@ -21,31 +21,28 @@ export const Login = (credentials, callback) => {
       done: res.data.error
         ? false
         : true,
-      error: res.data.error
+      error: res.data.error,
     });
   }).catch(err => {
-    callback({done: false, error: err});
+    callback({done: false, error: err,});
   });
-}).catch(err => {
-  callback({done: false, error: err,});
-});
 };
 
 export const Register = (user, callback) => {
-if (user.password !== user.passwordV) {
-  callback({loaded: true, done: false, error: "Passwords did not match!",});
-}
-axios.post(URL + "users/", {
-  ...user
-}).then(res => {
-  console.log("reg suc");
-  if (res.data.success) {
-    Login(user, callback);
-  } else {
-    callback({done: false, error: res.data.error,});
+  if (user.password !== user.passwordV) {
+    callback({loaded: true, done: false, error: "Passwords did not match!",});
   }
-}).catch(err => {
-  console.log("error", err);
-  callback({done: false, error: "Something bad happened, you are not supposed to see this",});
-});
+  axios.post(URL + "users/", {
+    ...user
+  }).then(res => {
+    console.log("reg suc");
+    if (!res.data.error) {
+      Login(user, callback);
+    } else {
+      callback({done: false, error: res.data.error,});
+    }
+  }).catch(err => {
+    console.log("error", err);
+    callback({done: false, error: "Something bad happened, you are not supposed to see this",});
+  });
 };
